5cc13ce27975ed68d5e443906ed2c596b7d62cd9
Jessica Lord <jessica.lord@github.com>
2014-04-03 15:13:11 -0700

    files files

---
+++ index.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+var exec = require('child_process').exec
+
+exec('git diff --name-only origin/gh-pages...', function(err, stdout, stdrr) {
+  var files = stdout.split('\n')
+  var command = ""
+  counter = 0
+  files.forEach(function(file) {
+    if (!file) return
+    if (counter === 0) {
+      command += 'Atom . ' + file
+    } else {
+      command += ' ' + file
+    }
+    counter++
+  })
+  exec(command)
+})
+
+// make it take in an arg for gh-pages or master branch
